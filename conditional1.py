d1fc72400820879f736cb6fef9e6c20c50a9f664
Fuy98 <79111629+Fuy98@users.noreply.github.com>
2023-03-30 18:52:03 -0600

    Add files via upload

---
+++ conditional1.py
@@ -0,0 +1,11 @@
+x = 1
+y = 10
+
+# Nested if statements
+if x < 10:
+    if y < 5:
+        print("x is less than 10 and y is less than 5")
+    elif y == 5:
+        print("x is less than 10 and y is equal to 5")
+    else:
+        print("x is less than 10 and y is greater than 5")
